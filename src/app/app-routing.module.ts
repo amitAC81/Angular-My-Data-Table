4ce3b5e36157281ab0a4955df5aba74f4ca5c675
Amit Chauhan <amit.chauhan@1rivet.local>
2020-02-04 17:52:39 +0530

    My first Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employee',
+    loadChildren: () => import('./employee/employee-routing.module').then(m => m.EmployeeRoutingModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
